6d3634e50e78925083bb2587e4d29a2beb348fdc
木仁 <wuyongwork@126.com>
2022-06-13 17:36:38 +0800

    third

--- midway-project/test/controller/home.test.ts
+++ midway-project/test/controller/home.test.ts
@@ -6,16 +6,19 @@ describe('test/controller/home.test.ts', () => {
   it('should GET /', async () => {
     // create app
     const app = await createApp<Framework>();
+    const startTime = new Date().getTime(); // 获取开始时间
 
     // make request
-    const result = await createHttpRequest(app).get('/');
+    const result = await createHttpRequest(app).get('/getData');
+    const endTime = new Date().getTime(); // 获取截止时间
 
     // use expect by jest
-    expect(result.status).toBe(200);
-    expect(result.text).toBe('Hello Midwayjs!');
+    expect(endTime - startTime).toBeLessThan(1000);
+    expect(result.text).toBe('//www.baidu.com/img/bd_logo1.png');
 
     // close app
     await close(app);
+
   });
 
 });
